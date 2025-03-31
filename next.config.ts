c0dacbfb010edd4ebfe733da3935ac8ee0a399dd
Fitsum1204 <fitsumzione24@gmail.com>
2025-03-31 10:00:47 +0300

    comment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
         images: {
           dangerouslyAllowSVG:true,
-          domains: ["images.unsplash.com"],
+      
           remotePatterns:[
             {
               protocol:'https',
