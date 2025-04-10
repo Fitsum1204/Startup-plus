f29e8ec9d0bbffb2d0b7af18fabfd821d6e17ee9
Fitsum1204 <fitsumzione24@gmail.com>
2025-04-10 09:31:01 +0300

    updating

--- next.config.ts
+++ next.config.ts
@@ -11,10 +11,10 @@ const nextConfig: NextConfig = {
             }
           ]
         },
-        experimental: {
+       /*  experimental: {
           ppr: "incremental", 
          
-        },
+        }, */
       
 };
 
